d37a515e0959be11e33f8044d5bb687a16a6a071
frectonz <fraol0912@gmail.com>
2026-06-08 19:05:13 +0300

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      forAllSystems =
+        fn:
+        let
+          systems = [
+            "x86_64-linux"
+            "aarch64-darwin"
+          ];
+          overlays = [ (import rust-overlay) ];
+        in
+        nixpkgs.lib.genAttrs systems (
+          system:
+          fn (
+            import nixpkgs {
+              inherit system overlays;
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs:
+      {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.bacon
+            pkgs.rust-analyzer
+            pkgs.rust-bin.stable.latest.default
+          ];
+        };
+      });
+
+      formatter = forAllSystems (
+        pkgs:
+        pkgs.treefmt.withConfig {
+          runtimeInputs = [ pkgs.nixfmt ];
+          settings = {
+            on-unmatched = "info";
+            formatter.nixfmt = {
+              command = "nixfmt";
+              includes = [ "*.nix" ];
+            };
+          };
+        }
+      );
+    };
+}
