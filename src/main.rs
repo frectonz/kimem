0e157f79d2ea332f20013df6c627e89df4336c16
frectonz <fraol0912@gmail.com>
2026-06-09 15:42:47 +0300

    feat: setup helix + setup initial args struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::net::IpAddr;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(default_value = "192.168.0.1")]
+    router: IpAddr,
+
+    #[clap(default_value = "admin")]
+    username: String,
+    #[clap(default_value = "admin")]
+    password: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    dbg!(&args);
 }
