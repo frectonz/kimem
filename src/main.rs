6a08e95935e2b600bdeabb80b1a20d69ecc6eba4
frectonz <fraol0912@gmail.com>
2026-06-09 16:09:02 +0300

    feat: implement fetching login nonce

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use clap::Parser;
+use serde::Deserialize;
 use std::net::IpAddr;
+
+type BoxStr = Box<str>;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -15,5 +18,55 @@ struct Args {
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    dbg!(&args);
+    let router: Router = args.into();
+
+    let nonce = router.fetch_nonce().await;
+    dbg!(nonce);
+}
+
+struct Router {
+    client: reqwest::Client,
+    address: String,
+    username: String,
+    password: String,
+}
+
+impl From<Args> for Router {
+    fn from(args: Args) -> Self {
+        Self {
+            client: reqwest::Client::builder()
+                .user_agent("Kimem CLI")
+                .build()
+                .unwrap(),
+            address: format!("http://{}", args.router),
+            username: args.username,
+            password: args.password,
+        }
+    }
+}
+
+impl Router {
+    async fn fetch_nonce(&self) -> BoxStr {
+        #[derive(Deserialize)]
+        struct NonceBody {
+            random_login: BoxStr,
+        }
+
+        let url = format!(
+            "{}/reqproc/proc_get?cmd=get_random_login&isTest=false",
+            self.address
+        );
+
+        let nonce = self
+            .client
+            .get(url)
+            .send()
+            .await
+            .unwrap()
+            .json::<NonceBody>()
+            .await
+            .unwrap();
+
+        nonce.random_login
+    }
 }
