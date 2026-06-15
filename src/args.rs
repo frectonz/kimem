a21daf46f8d3a40e7ff6a11216da8bf436a27106
frectonz <fraol0912@gmail.com>
2026-06-15 15:23:20 +0300

    feat: move CLI args to a separate file

---
+++ src/args.rs
@@ -0,0 +1,64 @@
+use crate::common::*;
+use clap::{Parser, Subcommand};
+use std::net::IpAddr;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+pub struct Args {
+    #[clap(short, long, default_value = "192.168.0.1")]
+    pub router: IpAddr,
+
+    #[clap(short, long, default_value = "admin")]
+    pub username: BoxStr,
+    #[clap(short, long, default_value = "admin")]
+    pub password: BoxStr,
+
+    #[command(subcommand)]
+    pub command: TopLevelCommands,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum TopLevelCommands {
+    /// Commands for fetching data from the router.
+    Get {
+        #[command(subcommand)]
+        command: GetCommands,
+    },
+    /// Commands for performing an action on the router.
+    Post {
+        #[command(subcommand)]
+        command: PostCommands,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+pub enum GetCommands {
+    /// List connected devices.
+    Devices,
+    /// Fetch router IMEI.
+    Imei,
+    /// Fetch SIM IMSI.
+    Imsi,
+    /// Fetch the network type.
+    NetworkType,
+    /// Fetch SIM PLMN.
+    Plmn,
+    /// Fetch router signal strength in dBm.
+    Rssi,
+    /// Fetch router signal bar.
+    Signalbar,
+    /// Fetch last cached airtime balance.
+    AirtimeBalance,
+    /// Fetch WAN IP address.
+    WanIpaddr,
+    /// Fetch PPP status.
+    PppStatus,
+    /// Fetch firmware version.
+    CrVersion,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum PostCommands {
+    /// Reboot the router.
+    Reboot,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+mod args;
 mod common;
 mod proc_get;
 mod proc_post;
 mod retry;
 mod router;
 
+pub use args::*;
 pub use common::*;
 pub use proc_get::*;
 pub use proc_post::*;
