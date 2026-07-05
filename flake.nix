4803cb508f43d865c9c92b09926c291041941da7
frectonz <fraol0912@gmail.com>
2026-07-05 21:26:21 +0300

    chore: switch to flake hub

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/*";
+    rust-overlay.url = "https://flakehub.com/f/oxalica/rust-overlay/*";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
