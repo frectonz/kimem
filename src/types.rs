eda0d7f56091a9081178601cfc097e063169082f
frectonz <fraol0912@gmail.com>
2026-09-09 00:17:31 +0300

    feat: parse `+0` timezones as valid

--- src/types.rs
+++ src/types.rs
@@ -428,12 +428,15 @@ pub struct Datetime {
 
 impl Datetime {
     /// The timezone part can be ignored because it is a lie. It
-    /// either says +8 or +12, but the actual time is always in GMT+3.
+    /// either says +8 or +12 or +0, but the actual time is always in GMT+3.
     /// Examples
     /// 26,06,05,13,06,24,+8
     /// 26,06,18,16,35,06,+12
     pub fn parse(datetime: &str) -> EyreResult<Self> {
-        let datetime = datetime.trim_end_matches(",+8").trim_end_matches(",+12");
+        let datetime = datetime
+            .trim_end_matches(",+0")
+            .trim_end_matches(",+8")
+            .trim_end_matches(",+12");
         let datetime = format!("{datetime},Africa/Addis_Ababa");
         let datetime = jiff::Zoned::strptime("%y,%m,%d,%H,%M,%S,%Q", datetime)?;
         Ok(Self { datetime })
